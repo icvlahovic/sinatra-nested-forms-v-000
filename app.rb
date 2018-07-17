8cd1d9b15a185370ce4b8e58aec6000bc7e4a7bc
Ian Vlahovic <banddorkfht7@gmail.com>
2018-07-17 20:35:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ship].each { |details| Ship.new(details) }
       erb :"pirates/show"
     end
 
